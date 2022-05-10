da64cd8c13d04fcaf3037f925184598bc0646e08
Nur Tahmid <noxana31@gmail.com>
2022-05-10 08:53:21 +0200

    removed 3 imports, pickle, multiprocessing and pip import main

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-from multiprocessing.connection import wait
 import time
-from pip import main
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
-import pickle
 from bs4 import BeautifulSoup
 
 
