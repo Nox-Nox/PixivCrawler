21be7a2878b56b249996cca06ec53c03d9e28b3e
Nur Tahmid <noxana31@gmail.com>
2022-05-10 12:53:23 +0200

    implemented function saved_login in lgit log

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import time
+import os.path
+from os import path
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -9,7 +11,7 @@ from bs4 import BeautifulSoup
 import pickle
 
 
-def login(driver):
+def new_login(driver):
     usernameField = '//*[@id="LoginComponent"]/form/div[1]/div[1]/input'
     passwordField = '//*[@id="LoginComponent"]/form/div[1]/div[2]/input'
     login_button = '//*[@id="LoginComponent"]/form/button'
@@ -26,6 +28,15 @@ def login(driver):
     pickle.dump(driver.get_cookies(), open("cookies.pkl", "wb"))
 
 
+def saved_login(driver):
+    driver.get("https://pixiv.net")
+    time.sleep(1)
+    cookies = pickle.load(open("cookies.pkl", "rb"))
+    for cookie in cookies:
+        driver.add_cookie(cookie)
+    driver.get("https://pixiv.net")
+
+
 def search():
     return 0
 
@@ -40,13 +51,7 @@ if __name__ == "__main__":
     s = Service('chromedriver_linux64 (1)/chromedriver')
     driver = webdriver.Chrome(service=s)
 
-    # login(driver)
-    driver.get("https://pixiv.net")
-    time.sleep(1)
-    cookies = pickle.load(open("cookies.pkl", "rb"))
-    for cookie in cookies:
-        driver.add_cookie(cookie)
-    driver.get("https://pixiv.net")
+    new_login(driver)
 
     artist_nick = input('enter artist name: ')
 # driver.find_element_by_xpath(search_bar).send_keys(artist, Keys.ENTER)
