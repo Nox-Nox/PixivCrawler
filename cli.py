6b93b9ddb26becb20abaa06a60ce3daf38c183a0
NurTahmid <nur.tahmid2022@gmail.com>
2022-06-10 05:33:29 +0200

    in cli.py removed the @options as they were useless

--- cli.py
+++ cli.py
@@ -9,14 +9,12 @@ def cli():
 
 
 @click.command('all', short_help='to download all illustrations of an artist, type/paste the artist id')
-@click.option('--userid', help="paste user id number found in the url")
 @click.argument('userid')
 def all(userid):
     all_download(userid)
 
 
 @click.command('single', short_help='to download a single illustration, type/paste the artwork id')
-@click.option('--artid', help="paste the id of the art found in the url")
 @click.argument('artid')
 def single(artid):
     single_download(artid)
